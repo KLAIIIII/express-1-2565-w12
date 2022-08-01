a222e3b91d83dec722d6182b89407c61ecf10860
KLAIIIII <guyyg03546@gmail.com>
2022-08-01 17:43:42 +0700

    Update

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express')
 const http = require('http')
-const { nextTick } = require('process')
-// Multiple routing
-const router = express.Router()
-const router2 = express.Router()
-const router3 = express.Router()
+const user = require('./routes/user')
+
 const app = express()
 
+app.use('/v1',user)
 
 const server = http.createServer(app)
-app.use(router,router2,router3)
-
-router.get('/',(req,res)=>{
-    res.send("Router Home")
-})
-
-router2.get('/user',(req,res)=>{
-    res.send('User Router Working')
-})
-
-router3.get('/admin',(req,res)=>{
-    res.send('Admin Router Working')
-})
 
 server.listen(5000,(err)=>{
-    if(err){
+    if(err)
         console.log(err)
-    }
+        console.log(`Server running at http://localhost:5000`)
+    
 })
